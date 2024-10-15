0b03e85c07ec0e6b58bd5d7ee41ffbc2c53cb7cc
Kfetahi.zip <121763997+K-sw-hw@users.noreply.github.com>
2024-10-15 11:03:18 +0200

    Update roboticHand.py

--- roboticHand.py
+++ roboticHand.py
@@ -1,1 +1,15 @@
 from data import gesture, distance, palm_center
+from pyedo import edo
+
+import time
+import math
+
+myedo = edo('192.168.12.1') # Crea instanza oggetto eDO
+init7Axes()
+
+if (gesture = "Mano aperta"):
+  moveGripper(70)
+else if (gesture = "Mano chiusa"):
+  moveGripper(10)
+else:
+  moveGripper(0)
